7b5acc0dc4b266eb0065f1655c50c1de52037c04
hendrik <hendrik.hamerlinck@hammernet.be>
2025-03-28 14:42:24 +0100

    control clock registers

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,52 @@
 #![no_std]
 #![no_main]
 
-use avr_device::attiny416::Peripherals;
+use avr_device::{
+    attiny416::Peripherals,
+    generic::{Reg, RegisterSpec, Resettable, Writable},
+};
 use panic_halt as _; // Import panic handler
 
-// Define clock frequency 
-const F_CPU: u32 = 10_000_000;
-
+// Define clock frequency
+const F_CPU: u32 = 20_000_000;
 // Simple delay function (not precise, for demo purposes)
 fn delay_ms(ms: u32) {
     // Rough approximation: adjust based on your clock speed
-    let cycles = (F_CPU / 1_000) * ms / 50; // ~50 cycles per loop
+    let cycles = (F_CPU / 1_000) * ms / 10; // ~ cycles per loop
     let mut bool: bool = true;
     for _ in 0..cycles {
         // Use a volatile write to prevent optimization
-        unsafe { core::ptr::write_volatile(&mut bool, true); }
+        unsafe {
+            core::ptr::write_volatile(&mut bool, true);
+        }
     }
+}
+
+fn protected_write<T>(dp: &Peripherals, reg: &Reg<T>, value: u8)
+where
+    T: Writable + Resettable,
+    T: RegisterSpec<Ux = u8>,
+{
+    // Write the CCP signature (0xD8) to enable protected register writes
+    dp.CPU.ccp.write(|w| unsafe { w.bits(0xD8) });
+
+    // Write the value to the register
+    reg.write(|w| unsafe { w.bits(value) });
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn main() -> ! {
     let dp = Peripherals::take().unwrap(); // Take ownership of peripherals
-    let portb = &dp.PORTB; // Reference to PORTB
 
+    // Initialize the clock
+    protected_write(&dp, &dp.SLPCTRL.ctrla, 0);
+    protected_write(&dp, &dp.CLKCTRL.osc32kctrla, 0); // RUNSTDBY disabled
+    protected_write(&dp, &dp.CLKCTRL.osc20mctrla, 0); // RUNSTDBY disabled
+    protected_write(&dp, &dp.CLKCTRL.mclkctrlb, 0); // Set Prescaler disabled
+    protected_write(&dp, &dp.CLKCTRL.mclkctrla, 0); // Set Prescaler disabled
+    protected_write(&dp, &dp.CLKCTRL.mclklock, 0b1); // LOCK CLOCK
+
+    let portb = &dp.PORTB; // Reference to PORTB
     portb.dirset.write(|w| w.pb5().set_bit()); // Set PB5 as output
 
     loop {
